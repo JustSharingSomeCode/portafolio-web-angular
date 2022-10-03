b07c35a1d171c31356fb1ad83f5ad4bdb94bd7a6
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-10-02 22:03:33 -0500

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IconsComponent } from './components/icons/icons.component';
 import { IconsContainerComponent } from './components/icons-container/icons-container.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IconsComponent,
-    IconsContainerComponent
+    IconsContainerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
