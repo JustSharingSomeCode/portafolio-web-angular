a72ad187dced6d882c22d90d53511fef38c1afe1
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-10-02 20:12:41 -0500

    icons border

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icons',
@@ -12,7 +12,9 @@ export class IconsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input()
   baseIcon : string = "devicon-angularjs-plain";
+
   colored : string = "";
 
   UpdateColor(value: boolean)
